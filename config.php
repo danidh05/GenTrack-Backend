2ebb771c39c7ff1029a79a2f9864fc4d6de21333
Daniel Dhaini <danidh20052005@gmail.com>
2026-05-10 14:48:35 +0300

    Stop tracking config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'db_host'              => '127.0.0.1:3307',
-    'db_name'              => 'gentrack',
-    'db_user'              => 'root',
-    'db_pass'              => 'Chelsea_2024',
-    'firebase_web_api_key' => 'AIzaSyD2j0tBdEvSqUnJ2IBwoPajb0XF_2Zrz48',
-];
